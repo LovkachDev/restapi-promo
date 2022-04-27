db1af267b4c1e65033c0436cd163f0f397ec2a26
Fedor <Lovkach@MacBook-Air-Lovkach.local>
2022-04-27 23:47:11 +0300

    work auth

--- admin/index.php
+++ admin/index.php
@@ -19,14 +19,38 @@
         <div class="container flex jcc" style="min-height: 90vh;">
             <div class="signup flex jcc aic">
                 <div class="prewiew__login">
-                    <form action="getRequest.php" method="post" class = "prewiew__form">
+                    <form action="index.php" method="post" class = "prewiew__form">
                         <p class = "form__label">Username</p>
                         <input class = "form__input" name = "login">
                         <p class = "form__label mar20">Password</p>
                         <input name = "password" type = "password" class = "form__input">
                         <button name= "submit" class = "form__button mar20">Войти</button>
                         <?php
+                            $submit = $_POST['submit'];
+                            if(isset($submit))
+                            {
+                                $url = 'http://localhost:8888/rest/?request=login';
+                                $data = array('login' => $_POST['login'], 'password' => $_POST['password']);
+                                
+                                $options = array(
+                                    'http' => array(
+                                        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                                        'method'  => 'POST',
+                                        'content' => http_build_query($data)
+                                    )
+                                );
+                                $context  = stream_context_create($options);
+                                $result = file_get_contents($url, false, $context);
+                                $response = json_decode($result, false);
                             
+                                if($response->status == "true")
+                                {
+                                    echo "success";
+                                }else
+                                {
+                                    echo "denied";
+                                }
+                            }
                         ?>
                     </form>
                 </div>
